1acbb97343c8bf7844cf5bd5d7000ca6ff5e0447
KEVIN01306 <kevin60sanchez31@gmail.com>
2026-04-14 07:28:58 -0600

    commit i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
+import { LANGUAGES } from './src/i18n/config.i18n';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
 
   site: 'https://kerrinfull.cloud',
   vite: {
@@ -14,10 +16,9 @@ export default defineConfig({
   },
 
   adapter: cloudflare(),
-  output: 'server',
   i18n: {
     defaultLocale: 'es',
-    locales: ['es','en'],
+    locales: Object.keys(LANGUAGES),
     routing: {
       prefixDefaultLocale: true,
       fallbackType: 'redirect'
---
+++ src/i18n/ui.i18n.ts
@@ -0,0 +1,18 @@
+export const ui = {
+    es: {
+        'nav.home': 'Inicio',
+        'nav.about': 'Nosotros',
+        'nav.products': 'Productos',
+        'nav.reviews': 'Reseñas',
+        'nav.branches': 'Sucursales',
+        'footer.rights': 'Todos los derechos reservados.',
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About Us',
+        'nav.products': 'Products',
+        'nav.reviews': 'Reviews',
+        'nav.branches': 'branches',
+        'footer.rights': 'All rights reserved.',
+    },
+} as const;
